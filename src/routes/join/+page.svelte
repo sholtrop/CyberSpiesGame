b7314dde4b20eca51d4a453c7e8e14a4503ffeb2
Nyandereian <nyandereian@gmail.com>
2023-11-02 13:40:57 +0100

    Add join lobby page where joining players will get checked on device compatibility too.

---
+++ src/routes/join/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { dev } from "$app/environment";
+    import { getSocketIO } from "$lib/websocket";
+    import { goto } from "$app/navigation";
+    import MainButton from "$lib/MainButton.svelte";
+    import Title from "$lib/Title.svelte";
+    let deviceSupported: boolean;
+  
+    function deviceIsSupported(): boolean {
+      return ("NDEFReader" in window && window.isSecureContext) || dev;
+    }
+  
+    function joinLobby() {
+      getSocketIO();
+      goto("/lobby", {replaceState: true});
+    }
+  
+    onMount(() => {
+      deviceSupported = deviceIsSupported();
+    });
+  </script>
+  
+  <div class="h-full flex flex-col justify-between items-center">
+    {#if deviceSupported}
+      <Title></Title>
+      <div>
+        <MainButton on:click={() => joinLobby()}>Join Lobby</MainButton>
+      </div>
+    {:else}
+      Your device is not supported.<br />Try Google Chrome on Android.
+    {/if}
+  </div>
+  
