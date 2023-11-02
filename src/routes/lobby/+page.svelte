c64f748f85131a197aa8ec8ff57e703f07bc9ddd
Sjors Holtrop <sjors@sholtrop.dev>
2023-11-02 19:30:22 +0100

    add colors

--- src/routes/lobby/+page.svelte
+++ src/routes/lobby/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {onMount} from 'svelte';
+  import { onMount } from "svelte";
   import { getSocketIO } from "$lib/websocket";
   import { goto } from "$app/navigation";
   import MainButton from "$lib/MainButton.svelte";
@@ -13,15 +13,17 @@
     { name: "Lochyin", color: "green" },
     { name: "Salih", color: "blue" },
     { name: "Amber", color: "yellow" },
-    { name: "Steef", color: "orange"},
-    { name: "Sjors", color: "red" }
+    { name: "Steef", color: "white" },
+    { name: "Sjors", color: "red" },
   ];
   const colors = {
-    red: "bg-red-700",
-    orange: "bg-orange-500",
+    red: "bg-red-600",
+    orange: "bg-orange-600",
     yellow: "bg-yellow-400",
     green: "bg-green-600",
     blue: "bg-blue-600",
+    purple: "bg-purple-700",
+    white: "bg-white",
   } as { [key: string]: string };
 
   function getRoomLink(): string {
@@ -36,36 +38,34 @@
     });
   }
 
-
   function addPlayer(name: string, color: string) {
-    players.push({name, color});
+    players.push({ name, color });
   }
 
   function startGame() {
     getSocketIO();
-    goto("/role", {replaceState: true});
+    goto("/role", { replaceState: true });
   }
 
   showRoomLink();
-
-
 </script>
+
 <div class="min-h-full h-1x flex flex-col justify-between">
   <div>
-    <Title></Title>
+    <Title />
     <div class="my-10">
-      <p bind:this={linkContainer}>Roomlink: </p>
-      <div bind:this={qrContainer}></div>
+      <p bind:this={linkContainer}>Roomlink:</p>
+      <div bind:this={qrContainer} />
     </div>
     <div>
       <h2>Players:</h2>
       <div bind:this={container}>
         {#each players as player}
-        <div class="flex items-baseline space-x-1.5">
-          <div class={colors[player.color] + " w-3 h-3"} />
-          <div>{player.name}</div>
-        </div>
-      {/each}
+          <div class="flex items-baseline space-x-1.5">
+            <div class={colors[player.color] + " w-3 h-3"} />
+            <div>{player.name}</div>
+          </div>
+        {/each}
       </div>
     </div>
   </div>
