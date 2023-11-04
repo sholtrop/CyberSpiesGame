d4b74d936e76b2c377fc065f9208ee52356a4778
Nyandereian <nyandereian@gmail.com>
2023-11-04 22:12:25 +0100

    Add vote timer and results

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -10,6 +10,9 @@
   let taskProgress: number = 50;
   let playerPick: string;
   $: voted = false;
+  $: resultsShown = false;
+  let voteTime = 50;
+  $: timer = voteTime;
 
   const players = [
     { name: "Lochyin", color: "green", status: "alive" },
@@ -19,20 +22,44 @@
     { name: "Sjors", color: "red", status: "-" },
   ];
 
+  let votes = {
+    Lochyin: 1,
+    Salih: 0,
+    Skip: 1
+  } as { [key: string]: number };
+
   function updateTaskBar(value: number) {
     taskProgress = value;
   }
 
   function handlePlayerPick(event: any) {
-    playerPick = event.target.textContent;
+    if (!voted && !resultsShown) playerPick = event.target.textContent;
   }
 
   function voteHandler() {
-    if (playerPick && !voted) {
+    if (playerPick && !voted && !resultsShown) {
       // register vote to server
       voted = true;
     }
   }
+
+  function showResults() {
+    resultsShown = true;
+    // Remove selection if not confirmed before results are shown.
+    if (playerPick && !voted) playerPick = "";
+  }
+
+  function startTimer() {
+    let interval = setInterval(() => {
+      if (browser) timer--;
+      if (timer <= 0) {
+        clearInterval(interval);
+        showResults();
+      }
+    }, 1000);
+  }
+
+  startTimer();
 </script>
 
 <div class="min-h-full flex flex-col justify-between">
@@ -50,16 +77,17 @@
         {/each}
       </div>
     </div>
-    <div class="flex flex-col gap-5 item-center">
-      <p class="font-bold text-lg">Vote off the spy!</p>
+    <div class="flex flex-col gap-5 {resultsShown ? "items-start" : ""}">
+      <p class="font-bold text-lg">{resultsShown ? "Voting results:" : "Vote off the spy!"}</p>
+      <progress id="voteTimerBar" value={timer} max={voteTime} class="w-full"></progress>
       {#each players as player}
         {#if player.status == "alive"}
-          <div on:click={handlePlayerPick} class="border border-green-600 px-6 py-4 text-center text-md {player.name == playerPick ? "bg-green-600" : ""}">
-            {player.name}
+          <div on:click={handlePlayerPick} class="{resultsShown ? "" : "border border-green-600"}  px-6 py-4 text-center text-md {player.name == playerPick ? "bg-green-600" : ""}">
+            {player.name}{resultsShown ? ": " + votes[player.name] : ""}
           </div>
         {/if}        
       {/each}
-      <div on:click={handlePlayerPick} class="border border-green-600 px-6 py-4 text-center text-md  {"Skip" == playerPick ? "bg-green-600" : ""}">Skip</div>
+      <div on:click={handlePlayerPick} class="{resultsShown ? "" : "border border-green-600"} px-6 py-4 text-center text-md  {"Skip" == playerPick ? "bg-green-600" : ""}">Skip{resultsShown ? ": " + votes.Skip : ""}</div>
     </div>
   </div>
   <div class="text-center">
