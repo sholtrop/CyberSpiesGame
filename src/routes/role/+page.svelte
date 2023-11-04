e81cc1db3e619a7134f5e1ae926a4a8a458477b5
Nyandereian <nyandereian@gmail.com>
2023-11-04 22:39:43 +0100

    Simplify code for Role page

--- src/routes/role/+page.svelte
+++ src/routes/role/+page.svelte
@@ -3,21 +3,9 @@
     import { onMount } from "svelte";
     import { browser } from '$app/environment';
 
-    let roleContainer: HTMLElement;
-
     function isSpy() {
-        let spy: boolean = true;
+        let spy: boolean = false;
         return spy;
-    }
-
-    function showRole() {
-        let role: string = "Cyber Criminal";
-        if (isSpy()) {
-            role = "Spy";
-        }
-        onMount(() => {
-            roleContainer.innerText = role;
-        });
     }
 
     function gotoGame() {
@@ -28,14 +16,13 @@
         }, 5000);
     }
 
-    showRole();
     gotoGame();
 
 
 </script>
 
 <div class="flex flex-col justify-center">
-    <p>Your role is: <span bind:this={roleContainer}></span></p>
+    <p>Your role is: <span>{isSpy() ? "Spy" : "Cyber Criminal"}</span></p>
     {#if isSpy()}
         <p>Your tasks are fake. Swipe up to see your special powers.</p>
     {/if}
