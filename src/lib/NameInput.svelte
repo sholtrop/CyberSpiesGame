3b7b60146b6b490f1856bb72b0f7d31391bf1d93
Sjors Holtrop <sjors@sholtrop.dev>
2023-11-07 19:54:28 +0100

    increase nameinptu size

--- src/lib/NameInput.svelte
+++ src/lib/NameInput.svelte
@@ -5,7 +5,12 @@
 
 <div>
   <label for="nameInput" class="block">Enter your name:</label>
-  <input id="nameInput" type="text" bind:value={playerName} class="text-black px-2">
+  <input
+    id="nameInput"
+    type="text"
+    bind:value={playerName}
+    class="text-black px-2 py-3 mt-1"
+  />
   {#if showError}
     <p class="text-red-500">Please enter a name.</p>
   {/if}
