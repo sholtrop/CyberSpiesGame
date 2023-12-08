49d039f53e19f0248ab152275c49133b183f0abb
Sjors Holtrop <sjors@sholtrop.dev>
2023-12-08 14:59:26 +0100

    fix cancel task

--- src/routes/minigame/+layout.svelte
+++ src/routes/minigame/+layout.svelte
@@ -1,6 +1,11 @@
+<script>
+  import { gotoReplace } from "$lib/util";
+</script>
+
 <div class="border-b w-full flex flex-col items-center pb-4 border-gray-300">
-  <button class="mt-4 w-1/2 px-3 py-2 bg-gray-800 rounded-sm text-gray-200"
-    >Cancel Task</button
+  <button
+    class="mt-4 w-1/2 px-3 py-2 bg-gray-800 rounded-sm text-gray-200"
+    on:click={() => gotoReplace("/game")}>Cancel Task</button
   >
 </div>
 <slot />
