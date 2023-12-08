0780a8ae778b441eb415fe624b0092f5c36dfbcd
Sjors Holtrop <sjors@sholtrop.dev>
2023-12-08 13:34:10 +0100

    dev

--- src/routes/role/+page.svelte
+++ src/routes/role/+page.svelte
@@ -20,7 +20,9 @@
 <div
   class="flex flex-col items-center justify-between min-h-screen space-y-10 py-10"
 >
-  <div class="flex flex-col items-center justify-end h-full mb-4 text-center p-10">
+  <div
+    class="flex flex-col items-center justify-end h-full mb-4 text-center p-10"
+  >
     <p class="mb-5 text-lg">
       Your role is: <span class="font-bold"
         >{$playerStore?.role.name === "crew"
@@ -30,14 +32,24 @@
     </p>
 
     {#if $playerStore?.role.name === "impostor"}
-      <p>You're an undercover spy infiltrating into a criminal den. Assassinate the criminals or retreive the data to win.</p>
-      <p><br>You have been granted special abilities. Swipe up on the next page to access them.</p>
-
+      <p>
+        You're an undercover spy infiltrating into a criminal den. Assassinate
+        the criminals or retreive the data to win.
+      </p>
+      <p>
+        <br />You have been granted special abilities. Swipe up on the next page
+        to access them.
+      </p>
     {:else}
-      <p>You're part of a cyber criminal organization. There is 1 spy amongst you.</p>
-      <p><br>Do your tasks to catch the spy or execute the spy during a meeting to win.</p>
+      <p>
+        You're part of a cyber criminal organization. There is 1 spy amongst
+        you.
+      </p>
+      <p>
+        <br />Do your tasks to catch the spy or execute the spy during a meeting
+        to win.
+      </p>
     {/if}
-
   </div>
   {#if $lobbyStore?.status.state === "roleExplanation"}
     <MainButton
@@ -48,8 +60,13 @@
         });
         goto("/game");
       }}
-      >Game will start in {$lobbyStore.status.countDown}<br />
-      <span class="text-sm text-gray-300">(Dev mode: Click to start now)</span>
+      >Game will start in {$lobbyStore.status.countDown}
+
+      {#if dev}
+        <br />
+        <span class="text-sm text-gray-300">(Dev mode: Click to start now)</span
+        >
+      {/if}
     </MainButton>
   {/if}
 </div>
